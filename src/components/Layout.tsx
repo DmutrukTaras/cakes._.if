7ff45dbd1c30f081f3f831a01b7c643329614db1
тарас дмитрик <DmutrukTaras>
2026-06-01 09:31:12 +0300

    scroll to top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import { ContactModalProvider } from "./ContactModal";
 import Footer from "./Footer";
 import Header from "./Header";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Layout() {
   return (
     <ContactModalProvider>
       <div className="min-h-screen overflow-hidden">
+        <ScrollToTop />
         <Header />
         <main className="pt-24 sm:pt-28">
           <Outlet />
